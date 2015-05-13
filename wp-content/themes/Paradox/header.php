aba2c3c4aa30145a3e9257c5e7350efab403895f
Ty Richards <me@tyrichards.com>
2015-05-13 03:56:06 -0500

    restore test

--- wp-content/themes/Paradox/header.php
+++ wp-content/themes/Paradox/header.php
@@ -66,7 +66,10 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>									
 						</button>
-	
+						<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+							<?php // echo esc_attr(get_bloginfo('name', 'display')); ?>
+							<img style="max-width:350px; margin-top: -9px;" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
+						</a>					
 					</div>						
 					<div class="collapse navbar-collapse navbar-primary-collapse">
 						<div class="btn-header">
